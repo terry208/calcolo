c5c497a2c3680b86fbdf021171c4ce0058df344b
terry208 <caela111073@hotmail.it>
2025-05-16 16:47:57 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	float n, m, s;
+	cout << "inserisci il primo numero \t" <<
+	endl;					//fine comand0
+	cin >> n;
+		cout << "inserisci il secondo numero \t" <<
+			endl;
+			cin >> m;
+			s= m+n;
+			cout << "la somma e:\n"<< s << endl;
+	return 0;
+}
